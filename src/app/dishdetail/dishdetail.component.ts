3bda16a9bb047900b397c26d728d0d5c386688f3
rifayetazamtalukder <rifayetewucse@gmail.com>
2020-06-04 20:33:59 +0600

    Animations Part 1

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -9,12 +9,33 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { Comment } from "../shared/comment";
 
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition
+} from "@angular/animations";
+
 
 
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
-  styleUrls: ['./dishdetail.component.scss']
+  styleUrls: ['./dishdetail.component.scss'],
+  animations: [
+    trigger('visibility', [
+      state('shown', style({
+        transform: 'scale(1.0)',
+        opacity: 1
+      })),
+      state('hidden', style({
+        transform: 'scale(0.5)',
+        opacity: 0
+      })),
+      transition('* => *', animate('0.5s ease-in-out'))
+    ])
+  ]
 })
 export class DishdetailComponent implements OnInit {
 
@@ -29,6 +50,7 @@ export class DishdetailComponent implements OnInit {
   commentForm: FormGroup;
   comment: Comment;
   dishCopy: Dish;
+  visibility = 'shown';
 
   @ViewChild('cform') commentFormDirective;
 
@@ -70,11 +92,15 @@ export class DishdetailComponent implements OnInit {
       .subscribe(dishIds => this.dishIds = dishIds);
 
     this.activatedRoute.params
-      .pipe(switchMap((params: Params) => this.dishService.getDish(params['id'])))
+      .pipe(switchMap((params: Params) => {
+        this.visibility = 'hidden';
+        return this.dishService.getDish(params['id']);
+      }))
       .subscribe(dish => {
         this.dish = dish;
         this.dishCopy = dish;
         this.setPrevNext(dish.id);
+        this.visibility = 'shown';
       },
         errmess => this.errMess = <any>errmess);
   }
